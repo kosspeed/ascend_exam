cde90c06b810a247df3b77fc317a9707bc56f9c7
Khwan Siricharoenporn <k.siricharoenporn@accenture.com>
2021-01-26 12:04:04 +0700

    Initial Commit

---
+++ ProductExam/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ProductExam
+//
+//  Created by k.siricharoenporn on 26/1/2564 BE.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
